1c1017d6ea1473c1196ff71b961c5063e5ec7e5a
Jaskiran <jaskiranchhokar@gmail.com>
2020-04-13 21:57:13 -0400

    add phraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,5 +2,20 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase(); 
     }
+
     phrase = this.phrase; 
+
+    addPhraseToDisplay() {
+        const ul = document.querySelector('#phrase > ul'); 
+        let phraseLetters = this.phrase.split(''); 
+        let li; 
+
+        phraseLetters.forEach(letter => {
+            li = document.createElement('li'); 
+            letter !== ' ' ? li.className = `hide letter ${letter}` : li.className = `space`;
+            li.innerHTML = letter;
+            ul.appendChild(li);
+        }); 
+        return ul;
+    }
 }
--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-const phrase = new Phrase('TESTING this new phrase'); 
 const game = new Game(); 
+const phrase = new Phrase(game.getRandomPhrase().phrase); 
 
-console.log(`phrase = ${phrase.phrase}`); 
 
-game.phrases.forEach((phrase,item) => {
-    console.log(`${item}: ${phrase.phrase}`); 
-}); 
+// const logPhrase = phrase => {
+//     console.log(phrase.phrase); 
+// }
 
-console.log(game.getRandomPhrase()); 
+// logPhrase(game.getRandomPhrase()); 
 
+
+console.log(phrase.addPhraseToDisplay());
