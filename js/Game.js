926ce42fdfecfffb6dc97964ff67bbf5ebe491d5
Jaskiran <jaskiranchhokar@gmail.com>
2020-04-17 01:00:57 -0400

    Add constructor to game class

--- js/Game.js
+++ js/Game.js
@@ -1,13 +1,15 @@
 class Game {
-    missed = 0; 
-    phrases = [
-        { phrase: 'On Cloud Nine' }, 
-        { phrase: 'Down To Earth'}, 
-        { phrase: 'Raining Cats and Dogs'},
-        { phrase: 'Easy As Pie'}, 
-        { phrase: 'A Dime a Dozen'}
-    ];
-    activePhrase = null;
+    constructor(missed,phrases,activePhrase) {
+        this.missed = 0; 
+        this.phrases = [
+            { phrase: 'On Cloud Nine' }, 
+            { phrase: 'Down To Earth'}, 
+            { phrase: 'Raining Cats and Dogs'},
+            { phrase: 'Easy As Pie'}, 
+            { phrase: 'A Dime a Dozen'}
+        ];
+        this.activePhrase = null;
+    }
     getRandomPhrase() {
         const phrases = this.phrases; 
         let randomIndex = Math.floor(Math.random() * phrases.length); 
