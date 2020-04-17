8d7e2ee443ba7339f30144b2588605c622e0192d
Jaskiran <jaskiranchhokar@gmail.com>
2020-04-16 23:49:06 -0400

    Clean up app.js

--- js/app.js
+++ js/app.js
@@ -4,31 +4,8 @@ document.querySelector('#btn__reset').addEventListener('click', e => {
     game.startGame();
 });
 
-const keyboard = document.querySelector('#qwerty');
-const key = document.querySelectorAll('.key');
-
-keyboard.addEventListener('click', e => {
+document.querySelector('#qwerty').addEventListener('click', e => {
     if(e.target.className === 'key') {
         game.handleInteraction(e.target); 
     }
 }); 
-
-
-
-
-// const keyboard = document.querySelector('#qwerty');
-// const phraseLetters = document.querySelectorAll('.letter');
-// let correctLetters = ''; 
-
-// keyboard.addEventListener('click', e => {
-//     // phraseLetters.forEach(phraseLetter => {
-//         game.handleInteraction(e.target.textContent); 
-
-//         // if(e.target.textContent === phraseLetter.textContent) {
-//         //     correctLetters += e.target.textContent; 
-//         //     this.showMatchedLetter(phraseLetter); 
-//         //     console.log('match: ' + e.target.textContent,phraseLetter.textContent); 
-//         //     return true;
-//         // } 
-//     // });            
-// });
